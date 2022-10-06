73e3e4c814906e99be61710189bba535cab4aaa2
rachel <rachel.tranchida@gmail.com>
2022-10-06 16:17:33 +0200

    changed x and y inversion

--- main.cpp
+++ main.cpp
@@ -21,10 +21,10 @@ int main() {
 vector<vector<int>> mandelbrotSet(vector<vector<int>> graphics) {
     for (int x = 0; x < graphics.size(); x++) {
         for (int y = 0; y < graphics[x].size(); y++) {
-            //y0 and x0 are reversed
-            int iteration = mandelbrot(y * ((2.47) / graphics[x].size()) - 2.0, -x * ((2.24) / graphics.size()) + 1.12);
+            int iteration = mandelbrot(x * ((2.47) / graphics.size()) - 2.0, -y * ((2.24) / graphics[x].size()) + 1.12);
             int codeCouleur = iteration == MAX_ITERATION ? 0 : 1;
-            graphics[x][y] = codeCouleur;
+            //x and y are reversed
+            graphics[y][x] = codeCouleur;
         }
     }
     return graphics;
