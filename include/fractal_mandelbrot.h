5d55119623d1eca35c2010b6c881721eae68dcff
rachel <rachel.tranchida@gmail.com>
2022-10-11 23:06:16 +0200

    add MAX_ITERATION as const static added function to calculate julia set and tricorn set

--- include/fractal_mandelbrot.h
+++ include/fractal_mandelbrot.h
@@ -10,9 +10,14 @@
 
 #include <vector>
 
+const static int MAX_ITERATION = 1000;
 int mandelbrot(double x0, double y0);
+int julia(double zx, double zy, double cx, double cy);
+int tricorn(double x0, double y0);
 
 std::vector<std::vector<int>> calcRect(double X1, double Y1, double X2, double Y2, int nX, int nY);
 std::vector<std::vector<int>> calcRect(double X1, double Y1, int nX, int nY, double dX, double dY);
 
+std::vector<std::vector<int>> tricornRect(double X1, double Y1, double X2, double Y2, int nX, int nY);
+std::vector<std::vector<int>> juliaRect(double cx, double cy, double R, int nX, int nY);
 #endif //MANDELBROT_FRACTAL_MANDELBROT_H
--- src/fractal_mandelbrot.cpp
+++ src/fractal_mandelbrot.cpp
@@ -19,7 +19,7 @@ int mandelbrot(double x0, double y0) {
     int iteration = 0;
     double r = 0.0; // Real number
     double i = 0.0; // Imaginary number
-    while ((r * r) + (i * i) <= 2 * 2 and iteration < 1000) {
+    while ((r * r) + (i * i) <= 2 * 2 and iteration < MAX_ITERATION) {
         double xtemp = (r * r) - (i * i) + x0;
         i = 2 * r * i + y0;
         r = xtemp;
@@ -68,4 +68,57 @@ vector<vector<int>> calcRect(double X1, double Y1, double X2, double Y2, int nX,
 vector<vector<int>> calcRect(double X1, double Y1, int nX, int nY, double dX, double dY) {
 
     return calcRect(X1 - dX / 2, Y1 - dY / 2.0, X1 + dX / 2.0, Y1 + dY / 2, nX, nY);
+}
+
+int tricorn(double x0, double y0) {
+    int iteration = 0;
+    double r = 0.0; // Real number
+    double i = 0.0; // Imaginary number
+    while ((r * r) + (i * i) < 2 * 2 and iteration < MAX_ITERATION) {
+        double xtemp = (r * r) - (i * i) + x0;
+        i = -(2 * r * i) + y0;
+        r = xtemp;
+
+        iteration++;
+    }
+    return iteration;
+}
+vector<vector<int>> tricornRect(double X1, double Y1, double X2, double Y2, int nX, int nY) {
+    vector<vector<int>> set(nX, vector<int>(nY, 0));
+
+    for(int x = 0; x < nX; x++) {
+        for(int y = 0; y < nY; y++) {
+            double x0 =  x * ((X2-X1) / nX) + X1;
+            double y0 =  y * ((Y2-Y1) / nY) + Y1;
+            set[x][y] = tricorn(x0,y0);
+        }
+    }
+
+    return set;
+
+}
+
+vector<vector<int>> juliaRect(double cx, double cy, double R, int nX, int nY) {
+    vector<vector<int>> set(nX, vector<int>(nY, 0));
+    for(int x = 0; x < nX; x++) {
+        for(int y = 0; y < nY; y++) {
+            double zx =  x * ((2*R) / nX) - R;
+            double zy =  y * ((2*R) / nY) -R;
+            set[x][y] = julia(zx,zy, cx, cy);
+        }
+    }
+
+    return set;
+}
+
+int julia(double zx, double zy, double cx, double cy) {
+    int iteration = 0;
+    while ((zx * zx) + (zy * zy) <= 2 * 2 and iteration < MAX_ITERATION) {
+        double xtemp = (zx * zx) - (zy * zy) + cx;
+        zy = 2 * zx * zy + cy;
+        zx = xtemp;
+
+        iteration++;
+    }
+    return iteration;
 }
