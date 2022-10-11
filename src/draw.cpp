f4c5c5279b9171dddc692ffcc43199c39d70f3ba
SuperQuentin <quentin.aellen@infrack.ch>
2022-10-11 22:44:47 +0200

    feat: 🎸 movement prototype

--- src/draw.cpp
+++ src/draw.cpp
@@ -17,12 +17,14 @@ using namespace std;
  * @param fractalSet
  */
 void draw(const vector <vector<int>> &fractalSet) {
+    string content = "";
     for (int x = 0; x < fractalSet.size(); x++) {
         for (int y = 0; y < fractalSet[x].size(); y++) {
-            cout << (fractalSet[x][y] == 1000 ? '0' : '1');
+            content += (fractalSet[x][y] == 1000 ? '0' : '1');
         }
-        cout << endl;
+        content += "\n";
     }
+    cout << content;
 }
 
 /**
@@ -31,13 +33,15 @@ void draw(const vector <vector<int>> &fractalSet) {
  * @param fractalSet
  */
 void drawGrayscale(const vector <vector<int>> &fractalSet) {
+    string content = "";
     for (int x = 0; x < fractalSet.size(); x++) {
         for (int y = 0; y < fractalSet[x].size(); y++) {
             int code = normalizeRange(fractalSet[y][x], 232, 255);
-            cout << "\x1b[38;5;" << to_string(code) << "m " << "*" << "\x1b[0m";
+            content += "\x1b[38;5;" + to_string(code) + "m  *\x1b[0m";
         }
-        cout << endl;
+        content += "\n";
     }
+    cout << content;
 }
 
 /**
@@ -46,11 +50,13 @@ void drawGrayscale(const vector <vector<int>> &fractalSet) {
  * @param fractalSet
  */
 void drawColors(const vector <vector<int>> &fractalSet) {
+    string content = "";
     for (int x = 0; x < fractalSet.size(); x++) {
         for (int y = 0; y < fractalSet[x].size(); y++) {
             int code = normalizeRange(fractalSet[y][x], 0, 232);
-            cout << "\x1b[38;5;" << to_string(code) << "m " << "*" << "\x1b[0m";
+            content += "\x1b[38;5;" + to_string(code) + "m *\x1b[0m";
         }
-        cout << endl;
+        content += "\n";
     }
+    cout << content;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 // - Aellen Quentin
 //
 
+#include <iostream>
+#include <cmath>
 #include "../include/fractal_mandelbrot.h"
 #include "../include/draw.h"
 #include "../include/util.h"
@@ -18,9 +20,54 @@ int main() {
 
     int width = 100, height = 100;
 
+    char input;
+    double x1 = -0.761574, y1 = -0.0847596;
+    unsigned long long int zoom = 1;
+
+    bool flag = true;
+
+    while (flag) {
+        system("cls");
+
+        drawColors(calcRect(x1, y1, width, height, 2.47 / zoom, 2.24 / zoom));
+
+        cout << "Navigation (w=Top, s=Bottom, a=Left, d=Right)\nZoom (+=Zoom in, -=Zoom out)\nExit (q)\n> ";
+
+        std::cin >> input;
+
+        switch (input) {
+            case 'w':
+                y1 -= 0.2 / zoom;
+                break;
+            case 's':
+                y1 += 0.2 / zoom;
+                break;
+            case 'a':
+                x1 -= 0.2 / zoom;
+                break;
+            case 'd':
+                x1 += 0.2 / zoom;
+                break;
+            case '+':
+                if(zoom < zoom*pow(2,64))
+                zoom *= 2;
+                break;
+            case '-':
+                if(zoom == 1) {
+                    break;
+                }
+                zoom /= 2;
+                break;
+            case 'q':
+                flag = false;
+                system("cls");
+                break;
+        }
+    }
+
     //draw(calcRect(-2, -1.12, 0.47, 1.12, width, height));
     //drawGrayscale(calcRect(-2, -1.12, 0.47, 1.12, width, height));
     //drawGrayscale(calcRect(-0.10936189822, 0.894921716226, width, height, 2.47 / 1000, 2.24 / 1000));
-    drawColors(calcRect(-0.10936189822, 0.894921716226, width, height, 2.47 / 1000, 2.24 / 1000));
+    //drawColors(calcRect(-0.10936189822, 0.894921716226, width, height, 2.47 / 1000, 2.24 / 1000));
     return 0;
 }
